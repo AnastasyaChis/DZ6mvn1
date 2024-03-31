3d55babedb0b3b2557d15de9a65bb6268cb5463d
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-03-31 23:13:31 +0300

    Initial commit

---
+++ src/test/java/FreelancerServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+class FreelancerServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void relaxTest1(int expected, int income, int expenses, int threshold) {
+        FreelancerService service = new FreelancerService();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
